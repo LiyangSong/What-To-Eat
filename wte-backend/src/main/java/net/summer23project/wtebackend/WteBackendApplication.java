eb5b63ade943bd85aec19cf449912fdab224d893
LiyangSong <song.liy@northeastern.edu>
2023-08-03 19:05:21 -0400

    initial framework for both back and front end

---
+++ wte-backend/src/main/java/net/summer23project/wtebackend/WteBackendApplication.java
@@ -0,0 +1,13 @@
+package net.summer23project.wtebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WteBackendApplication.class, args);
+	}
+
+}
