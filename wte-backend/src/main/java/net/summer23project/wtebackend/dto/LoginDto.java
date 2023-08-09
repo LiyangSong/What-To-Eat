772a1c87030d663254a5b868b74fcde7811e6e8d
LiyangSong <song.liy@northeastern.edu>
2023-08-09 01:02:58 -0400

    create LoginDto

---
+++ wte-backend/src/main/java/net/summer23project/wtebackend/dto/LoginDto.java
@@ -0,0 +1,18 @@
+package net.summer23project.wtebackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author Liyang
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginDto {
+    private String nameOrEmail;
+    private String password;
+}
