5466df26d44890067c100acd5a18113fc068e5ea
LiyangSong <song.liy@northeastern.edu>
2023-08-09 01:08:36 -0400

    add find method for repository class

--- wte-backend/src/main/java/net/summer23project/wtebackend/repository/IngredientRepository.java
+++ wte-backend/src/main/java/net/summer23project/wtebackend/repository/IngredientRepository.java
@@ -3,5 +3,10 @@ package net.summer23project.wtebackend.repository;
 import net.summer23project.wtebackend.entity.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * @author Liyang
+ */
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+    Ingredient findIngredientById(Long id);
+    Ingredient findIngredientByName(String name);
 }
