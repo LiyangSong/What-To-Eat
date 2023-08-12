ab65e6977c8eafcbd9d4c98d99ed98b195bb62a8
LiyangSong <song.liy@northeastern.edu>
2023-08-12 02:58:03 -0400

    replace entity with Optional<>

--- wte-backend/src/main/java/net/summer23project/wtebackend/repository/IngredientRepository.java
+++ wte-backend/src/main/java/net/summer23project/wtebackend/repository/IngredientRepository.java
@@ -3,10 +3,12 @@ package net.summer23project.wtebackend.repository;
 import net.summer23project.wtebackend.entity.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
  * @author Liyang
  */
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
-    Ingredient findIngredientById(Long id);
-    Ingredient findIngredientByName(String name);
+    Optional<Ingredient> findById(Long id);
+    Optional<Ingredient> findByName(String name);
 }
