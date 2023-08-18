6be317c031ec6e1849cc7564c71a85c8c8a6b9b4
LiyangSong <song.liy@northeastern.edu>
2023-08-18 05:59:38 -0400

    implement createDish API

--- wte-backend/src/main/java/net/summer23project/wtebackend/controller/DishController.java
+++ wte-backend/src/main/java/net/summer23project/wtebackend/controller/DishController.java
@@ -2,12 +2,12 @@ package net.summer23project.wtebackend.controller;
 
 import lombok.AllArgsConstructor;
 import net.summer23project.wtebackend.dto.DishDto;
-import net.summer23project.wtebackend.entity.Dish;
-import net.summer23project.wtebackend.entity.User;
+import net.summer23project.wtebackend.dto.DishIngredientAmountDto;
 import net.summer23project.wtebackend.exception.ApiException;
-import net.summer23project.wtebackend.repository.DishRepository;
-import net.summer23project.wtebackend.repository.UserRepository;
+import net.summer23project.wtebackend.mapper.DishRequestMapper;
+import net.summer23project.wtebackend.service.DishIngredientAmountService;
 import net.summer23project.wtebackend.service.DishService;
+import net.summer23project.wtebackend.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -16,9 +16,10 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 /**
- * @author Liyang
+ * @author Yue, Liyang
  */
 @RestController
 @RequestMapping("/api/dishes")
@@ -26,16 +27,29 @@ import java.util.List;
 @AllArgsConstructor
 public class DishController {
     private DishService dishService;
+    private UserService userService;
+    private DishIngredientAmountService dishIngredientAmountService;
+    private DishRequestMapper dishRequestMapper;
 
-    // Build Add Dish REST API, test url: Post 127.0.0.1:8080/api/dishes
-    @PostMapping
+    // http://localhost:8080/api/dishes/create
+    @PostMapping("/create")
     @Transactional(rollbackFor = ApiException.class)
     public ResponseEntity<DishDto> createDish(
             @AuthenticationPrincipal UserDetails userDetails,
-            @RequestBody DishDto dishDto) {
+            @RequestBody Map<String, Object> requestBody) {
+
+        DishDto dishDto = dishRequestMapper.mapToDishDto(requestBody);
+        DishDto savedDishDto = dishService.createDish(dishDto);
+
         String userName = userDetails.getUsername();
-        DishDto savedDish=dishService.createDish(dishDto,userName);
-        return new ResponseEntity<>(savedDish, HttpStatus.CREATED);
+        userService.updateUserWithAddedDish(userName, savedDishDto);
+
+        List<DishIngredientAmountDto> dishIngredientAmountDtos = dishRequestMapper.mapToDishIngredientAmountDtos(requestBody);
+        for (DishIngredientAmountDto dishIngredientAmountDto : dishIngredientAmountDtos) {
+            dishIngredientAmountService.createDishIngredientAmount(dishIngredientAmountDto);
+        }
+
+        return new ResponseEntity<>(savedDishDto, HttpStatus.CREATED);
     }
 
     @GetMapping("{id}")
