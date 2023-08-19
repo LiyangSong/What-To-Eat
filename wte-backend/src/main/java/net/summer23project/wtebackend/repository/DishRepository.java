209bec03b0f4aed37cd1ce39d43afe15d6d12fe3
LiyangSong <song_ly@outlook.com>
2023-08-18 22:06:42 -0400

    remove unused import

--- wte-backend/src/main/java/net/summer23project/wtebackend/repository/DishRepository.java
+++ wte-backend/src/main/java/net/summer23project/wtebackend/repository/DishRepository.java
@@ -1,11 +1,8 @@
 package net.summer23project.wtebackend.repository;
 
 import net.summer23project.wtebackend.entity.Dish;
-import net.summer23project.wtebackend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
 import java.util.Optional;
 
 /**
