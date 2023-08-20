93a4fd9caa34794f5c9648ada743c121cc0e71d7
LiyangSong <song.liy@northeastern.edu>
2023-08-20 04:19:02 -0400

    apply ingredientDetailsDto in ingredientController

--- wte-backend/src/main/java/net/summer23project/wtebackend/controller/IngredientController.java
+++ wte-backend/src/main/java/net/summer23project/wtebackend/controller/IngredientController.java
@@ -1,10 +1,11 @@
 package net.summer23project.wtebackend.controller;
 
 import lombok.AllArgsConstructor;
+import net.summer23project.wtebackend.dto.IngredientDetailsDto;
 import net.summer23project.wtebackend.dto.IngredientDto;
 import net.summer23project.wtebackend.dto.IngredientNutrientAmountDto;
 import net.summer23project.wtebackend.exception.ApiException;
-import net.summer23project.wtebackend.mapper.IngredientRequestMapper;
+import net.summer23project.wtebackend.mapper.IngredientDetailsMapper;
 import net.summer23project.wtebackend.service.IngredientNutrientAmountService;
 import net.summer23project.wtebackend.service.IngredientService;
 import org.springframework.http.HttpStatus;
@@ -15,8 +16,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 
 /**
@@ -29,22 +30,28 @@ import java.util.Map;
 public class IngredientController {
     private IngredientService ingredientService;
     private IngredientNutrientAmountService ingredientNutrientAmountService;
-    private IngredientRequestMapper ingredientRequestMapper;
+    private IngredientDetailsMapper ingredientDetailsMapper;
 
     // Post http://localhost:8080/api/ingredients
     @PostMapping
     @Transactional(rollbackFor = ApiException.class)
-    public ResponseEntity<IngredientDto> createIngredient(
-            @RequestBody Map<String, Object> requestBody) {
+    public ResponseEntity<IngredientDetailsDto> createIngredient(
+            @RequestBody IngredientDetailsDto ingredientDetailsDto) {
 
-        IngredientDto ingredientDto = ingredientRequestMapper.mapToIngredientDto(requestBody);
+        IngredientDto ingredientDto = ingredientDetailsMapper.mapToIngredientDto(ingredientDetailsDto);
         IngredientDto savedIngredientDto = ingredientService.createIngredient(ingredientDto);
 
-        List<IngredientNutrientAmountDto> ingredientNutrientAmountDtos = ingredientRequestMapper.mapToIngredientNutrientAmountDtos(requestBody);
+        List<IngredientNutrientAmountDto> ingredientNutrientAmountDtos = ingredientDetailsMapper.mapToIngredientNutrientAmountDtos(ingredientDetailsDto);
+        List<IngredientNutrientAmountDto> savedIngredientNutrientAmountDtos = new ArrayList<>();
         for (IngredientNutrientAmountDto ingredientNutrientAmountDto : ingredientNutrientAmountDtos) {
-            ingredientNutrientAmountService.createIngredientNutrientAmount(ingredientNutrientAmountDto);
+            IngredientNutrientAmountDto savedIngredientNutrientAmountDto = ingredientNutrientAmountService.createIngredientNutrientAmount(ingredientNutrientAmountDto);
+            savedIngredientNutrientAmountDtos.add(savedIngredientNutrientAmountDto);
         }
 
-        return new ResponseEntity<>(savedIngredientDto, HttpStatus.CREATED);
+        IngredientDetailsDto savedIngredientDetailsDto = ingredientDetailsMapper.mapToIngredientDetailsDto(
+                savedIngredientDto, savedIngredientNutrientAmountDtos
+        );
+
+        return new ResponseEntity<>(savedIngredientDetailsDto, HttpStatus.CREATED);
     }
 }
