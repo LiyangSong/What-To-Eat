b7d8447e3e08f152143ab3a22df3e124f04d99ce
LiyangSong <song.liy@northeastern.edu>
2023-08-22 02:19:50 -0400

    add existence verification

--- wte-backend/src/main/java/net/summer23project/wtebackend/repository/DishIngredientAmountRepository.java
+++ wte-backend/src/main/java/net/summer23project/wtebackend/repository/DishIngredientAmountRepository.java
@@ -11,4 +11,5 @@ import java.util.Optional;
  */
 public interface DishIngredientAmountRepository extends JpaRepository<DishIngredientAmount, Long> {
     Optional<List<DishIngredientAmount>> findByDishId(Long dishId);
+    Boolean existsByDishIdAndIngredientId(Long dishId, Long ingredientId);
 }
