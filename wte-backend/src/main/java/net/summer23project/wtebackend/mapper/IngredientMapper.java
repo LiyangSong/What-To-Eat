0a200cb60978baeb17d43f1783e8bfa05a4a70a1
LiyangSong <song.liy@northeastern.edu>
2023-08-26 18:12:56 -0400

    edit ingredient related mappers

--- wte-backend/src/main/java/net/summer23project/wtebackend/mapper/IngredientMapper.java
+++ wte-backend/src/main/java/net/summer23project/wtebackend/mapper/IngredientMapper.java
@@ -1,6 +1,8 @@
 package net.summer23project.wtebackend.mapper;
 
 import lombok.AllArgsConstructor;
+import net.summer23project.wtebackend.dto.IngredientReturnDto;
+import net.summer23project.wtebackend.entity.Ingredient;
 import org.springframework.stereotype.Component;
 
 /**
@@ -9,30 +11,12 @@ import org.springframework.stereotype.Component;
 @Component
 @AllArgsConstructor
 public class IngredientMapper {
-    //private final UnitRepository unitRepository;
-    //private final IngredientRepository ingredientRepository;
-    //
-    //public IngredientDto mapToIngredientDto(Ingredient ingredient) {
-    //    return new IngredientDto(
-    //            ingredient.getName(),
-    //            ingredient.getUnit().getName()
-    //    );
-    //}
-    //
-    //public Ingredient mapToIngredient(IngredientDto ingredientDto) {
-    //    String ingredientName = ingredientDto.getName();
-    //    if(ingredientRepository.existsByName(ingredientName)) {
-    //        throw new ApiException(HttpStatus.CONFLICT, "Ingredient already exists with given ingredientName: " + ingredientName);
-    //    }
-    //
-    //    Ingredient ingredient = new Ingredient();
-    //    ingredient.setName(ingredientName);
-    //
-    //    String unitName = ingredientDto.getUnitName();
-    //    Unit unit = unitRepository.findByName(unitName)
-    //                    .orElseThrow(() -> new ApiException(HttpStatus.BAD_REQUEST, "Unit does not exist with given unitName: " + unitName));
-    //    ingredient.setUnit(unit);
-    //
-    //    return ingredient;
-    //}
+
+    public IngredientReturnDto mapToIngredientReturnDto(Ingredient ingredient) {
+        return new IngredientReturnDto(
+                ingredient.getId(),
+                ingredient.getName(),
+                ingredient.getUnit().getName()
+        );
+    }
 }
--- wte-backend/src/main/java/net/summer23project/wtebackend/mapper/UserIngredientInventoryMapper.java
+++ wte-backend/src/main/java/net/summer23project/wtebackend/mapper/UserIngredientInventoryMapper.java
@@ -2,14 +2,7 @@ package net.summer23project.wtebackend.mapper;
 
 import lombok.AllArgsConstructor;
 import net.summer23project.wtebackend.dto.UserIngredientInventoryReturnDto;
-import net.summer23project.wtebackend.entity.Ingredient;
-import net.summer23project.wtebackend.entity.User;
 import net.summer23project.wtebackend.entity.UserIngredientInventory;
-import net.summer23project.wtebackend.exception.ApiException;
-import net.summer23project.wtebackend.repository.IngredientRepository;
-import net.summer23project.wtebackend.repository.UserIngredientInventoryRepository;
-import net.summer23project.wtebackend.repository.UserRepository;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 /**
@@ -18,42 +11,15 @@ import org.springframework.stereotype.Component;
 @Component
 @AllArgsConstructor
 public class UserIngredientInventoryMapper {
-    //private final UserRepository userRepository;
-    //private final IngredientRepository ingredientRepository;
-    //private final UserIngredientInventoryRepository userIngredientInventoryRepository;
-    //
-    //public UserIngredientInventoryReturnDto mapToUserIngredientInventoryDto(
-    //        UserIngredientInventory userIngredientInventory) {
-    //
-    //    return new UserIngredientInventoryReturnDto(
-    //            userIngredientInventory.getUser().getName(),
-    //            userIngredientInventory.getIngredient().getName(),
-    //            userIngredientInventory.getIngredientAmount()
-    //    );
-    //}
-    //
-    //public UserIngredientInventory mapToUserIngredientInventory(
-    //        UserIngredientInventoryReturnDto userIngredientInventoryReturnDto) {
-    //
-    //    String userName = userIngredientInventoryReturnDto.getUserName();
-    //    User user = userRepository.findByName(userName)
-    //            .orElseThrow(() -> new ApiException(HttpStatus.BAD_REQUEST, "User does not exist with given userName: " + userName));
-    //    Long userId = user.getId();
-    //
-    //    String ingredientName = userIngredientInventoryReturnDto.getIngredientName();
-    //    Ingredient ingredient = ingredientRepository.findByName(ingredientName)
-    //            .orElseThrow(() -> new ApiException(HttpStatus.BAD_REQUEST, "Ingredient does not exist with given ingredientName: " + ingredientName));
-    //    Long ingredientId = ingredient.getId();
-    //
-    //    if (userIngredientInventoryRepository.existsByUserIdAndIngredientId(userId, ingredientId)) {
-    //        throw new ApiException(HttpStatus.CONFLICT, "UserUserInventory already exists with given userName: " + userName + " and ingredientName: " + ingredientName);
-    //    }
-    //
-    //    UserIngredientInventory userIngredientInventory = new UserIngredientInventory();
-    //    userIngredientInventory.setUser(user);
-    //    userIngredientInventory.setIngredient(ingredient);
-    //    userIngredientInventory.setIngredientAmount(userIngredientInventoryReturnDto.getIngredientAmount());
-    //
-    //    return userIngredientInventory;
-    //}
+
+    public UserIngredientInventoryReturnDto mapToUserIngredientInventoryReturnDto(
+            UserIngredientInventory userIngredientInventory) {
+
+        return new UserIngredientInventoryReturnDto(
+                userIngredientInventory.getUser().getName(),
+                userIngredientInventory.getIngredient().getId(),
+                userIngredientInventory.getIngredient().getName(),
+                userIngredientInventory.getIngredientAmount()
+        );
+    }
 }
