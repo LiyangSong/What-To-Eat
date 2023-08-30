6ca019756ea61731f502b5b73772a49137a14fd9
LiyangSong <song.liy@northeastern.edu>
2023-08-30 07:45:24 -0400

    config axios to include token in request

---
+++ wte-frontend/src/services/axiosInstance.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import {getToken} from "./UserService.js";
+
+const axiosInstance = axios.create();
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = getToken();
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default axiosInstance;
