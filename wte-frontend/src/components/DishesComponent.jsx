e270bbcefc05b8dcd90f003703c15fc131fc43af
LiyangSong <song.liy@northeastern.edu>
2023-08-30 07:46:46 -0400

    test api connection

--- wte-frontend/src/components/DishesComponent.jsx
+++ wte-frontend/src/components/DishesComponent.jsx
@@ -1,5 +1,41 @@
-function DishesComponent(){
-    return null;
+import { useState, useEffect } from 'react';
+import { getDishById } from "../services/DishService.js";
+import {loginApiCall, storeToken} from "../services/UserService.js";
+
+function DishesComponent() {
+    const [dishDetailsReturnDto, setDishDetailsReturnDto] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const token = loginApiCall({"nameOrEmail": "keke", "password": "password"})
+                storeToken(token);
+                const dishId = 1;
+                const response = await getDishById(dishId);
+                setDishDetailsReturnDto(response.data);
+            } catch (error) {
+                setError(error);
+            }
+        })();
+    }, []);
+
+    return (
+        <div>
+            <h1>Test Get Dish By ID</h1>
+            {error && <p>Error: {JSON.stringify(error)}</p>}
+            {dishDetailsReturnDto ? (
+                <div>
+                    <h2>Dish Details</h2>
+                    <p>ID: {dishDetailsReturnDto.dishId}</p>
+                    <p>Name: {dishDetailsReturnDto.dishName}</p>
+                    <p>Ingredient Amounts: {dishDetailsReturnDto.ingredientAmountMaps}</p>
+                </div>
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
+    );
 }
 
 export default DishesComponent;
