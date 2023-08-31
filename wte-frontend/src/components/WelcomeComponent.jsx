b9cd74762680370d7a57aea9800726fd347862f0
LiyangSong <song.liy@northeastern.edu>
2023-08-31 01:25:49 -0400

    test getDish api

--- wte-frontend/src/components/WelcomeComponent.jsx
+++ wte-frontend/src/components/WelcomeComponent.jsx
@@ -1,8 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 function WelcomeComponent(){
+    const navigator = useNavigate();
     return (
         <div>
-            <button>Login</button>
-            <button>Register</button>
+            <h2>WELCOME!</h2>
+            <button onClick={() => navigator("/login")}>Login</button>
+            <button onClick={() => navigator("/register")}>Register</button>
         </div>
 
     );
