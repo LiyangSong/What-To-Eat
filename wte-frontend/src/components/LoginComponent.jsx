8287cb144bb031339ec743d6dcb7f3b5791c3216
LiyangSong <song.liy@northeastern.edu>
2023-08-31 01:12:08 -0400

    initialize login form

--- wte-frontend/src/components/LoginComponent.jsx
+++ wte-frontend/src/components/LoginComponent.jsx
@@ -1,5 +1,86 @@
+import { loginApiCall, saveLoggedInUser, storeToken } from "../services/UserService.js";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 function LoginComponent(){
-    return null;
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const navigator = useNavigate();
+
+    async function handleLoginForm(e) {
+        e.preventDefault();
+        await loginApiCall({nameOrEmail:username, password:password})
+            .then((response) => {
+                console.log(response.data);
+
+                const token = 'Basic ' + window.btoa(username + ":" + password);
+                storeToken(token);
+                saveLoggedInUser(username);
+
+                navigator("/dishes")
+                window.location.reload(false);
+            }).catch(error => {
+                console.error(error);
+            })
+    }
+
+    return (
+        <div className='container'>
+            <br /> <br />
+            <div className='row'>
+                <div className='col-md-6 offset-md-3'>
+                    <div className='card'>
+                        <div className='card-header'>
+                            <h2 className='text-center'> Login Form </h2>
+                        </div>
+
+                        <div className='card-body'>
+                            <form>
+                                <div className='row mb-3'>
+                                    <label className='col-md-3 control-label'> Username or Email</label>
+                                    <div className='col-md-9'>
+                                        <input
+                                            type='text'
+                                            name='username'
+                                            className='form-control'
+                                            placeholder='Enter username'
+                                            value={username}
+                                            onChange={ (e) => setUsername(e.target.value)}
+                                        >
+                                        </input>
+                                    </div>
+                                </div>
+
+                                <div className='row mb-3'>
+                                    <label className='col-md-3 control-label'> Password </label>
+                                    <div className='col-md-9'>
+                                        <input
+                                            type='password'
+                                            name='password'
+                                            className='form-control'
+                                            placeholder='Enter password'
+                                            value={password}
+                                            onChange={ (e) => setPassword(e.target.value)}
+                                        >
+                                        </input>
+                                    </div>
+                                </div>
+
+                                <div className='form-group mb-3'>
+                                    <button
+                                        className='btn btn-primary'
+                                        onClick={ (e) => handleLoginForm(e)}
+                                    >
+                                        Submit
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default LoginComponent;
