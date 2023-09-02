dc3ab8224592573b159b7d5e2f135e063eb08977
LiyangSong <song.liy@northeastern.edu>
2023-09-02 04:15:10 -0400

    initialize Dish component

--- wte-frontend/src/components/DishesComponent.jsx
+++ wte-frontend/src/components/DishesComponent.jsx
@@ -1,46 +1,41 @@
 import { useState, useEffect } from 'react';
-import { getDishById } from "../services/DishService.js";
+import {getAllDishes, getDishById} from "../services/DishService.js";
+import {useNavigate} from "react-router-dom";
 
 function DishesComponent() {
-    const [dishDetailsReturnDto, setDishDetailsReturnDto] = useState(null);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        (async () => {
-            try {
-                const dishId = 1;
-                const response = await getDishById(dishId);
-                setDishDetailsReturnDto(response.data);
-            } catch (error) {
-                setError(error);
-            }
-        })();
-    }, []);
+    const navigator = useNavigate();
+    const [dishes, setDishes] = useState([]);
+    const listDishes = () => {
+        getAllDishes()
+            .then(response => response.data)
+            .catch(error => console.error(error))
+    }
 
     return (
-        <div>
-            <h1>Test Get Dish By ID</h1>
-            {error && <p>Error: {JSON.stringify(error)}</p>}
-            {dishDetailsReturnDto ? (
-                <div>
-                    <h4>Dish Details</h4>
-                    <p>ID: {dishDetailsReturnDto.dishId.toString()}</p>
-                    <p>Name: {dishDetailsReturnDto.dishName.toString()}</p>
-                    <p>Ingredient Amounts:</p>
-                    <p>
-                        {dishDetailsReturnDto.ingredientAmountMaps.map(map =>
-                            <div key={map.ingredientId.toString()}>
-                                <p>Ingredient Name: {map.ingredientName.toString()}</p>
-                                <p>Ingredient Amount: {map.ingredientAmount.toString()}</p>
-                            </div>
-                        )}
-                    </p>
-                </div>
-            ) : (
-                <p>Loading...</p>
-            )}
+        <div className="container">
+            <br />
+            <table className="table table-bordered table-striped">
+                <thead>
+                TODO
+                </thead>
+                <tbody>
+                TODO
+                </tbody>
+            </table>
+            <br />
+            <div className="row">
+                <button
+                    onClick={()=>navigator("/dish-add")}>
+                    Add Dish
+                </button>
+                <button
+                    onClick={()=>navigator("/dish-create")}
+                >
+                    Create Dish
+                </button>
+            </div>
         </div>
-    );
+    )
 }
 
 export default DishesComponent;
